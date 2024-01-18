c7ed524cf6568d20fb8ef46e09751df8c7ebffcb
Tommy Juhnke <dreadwarrior@users.noreply.github.com>
2024-01-18 19:56:50 +0100

    Update to de.creativecouple.validation:fast-isbn:1.1.2

--- script/isbn13.java
+++ script/isbn13.java
@@ -1,5 +1,5 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
-//DEPS de.creativecouple.validation:fast-isbn:1.1.1
+//DEPS de.creativecouple.validation:fast-isbn:1.1.2
 
 import de.creativecouple.validation.isbn.ISBN;
 import static java.lang.System.*;
