891a67f04aca1a8e86f7f20e092b7b3777cd23d0
Thomas Juhnke <tommy@van-tomas.de>
2024-10-17 19:51:17 +0200

    Update creativecouple/fast-isbn to v1.2.2

--- script/isbn13.java
+++ script/isbn13.java
@@ -1,13 +1,12 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
-//DEPS net.creativecouple.validation:fast-isbn:1.2.1
+//DEPS net.creativecouple.validation:fast-isbn:1.2.2
 
 import net.creativecouple.validation.isbn.ISBN;
-import static java.lang.System.*;
 
 public class isbn13 {
 
     public static void main(String... args) {
         var isbn = ISBN.valueOf(args[0]);
-        out.println(isbn.toString());
+        System.out.println(isbn.toString());
     }
 }
