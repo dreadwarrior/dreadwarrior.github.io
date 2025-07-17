7322c3052133c4e11655f50b9d4d40f6d26bd7ac
Tommy Juhnke <dreadwarrior@users.noreply.github.com>
2025-07-17 22:26:03 +0200

    Update fast-isbn to v1.2.20

--- script/isbn13.java
+++ script/isbn13.java
@@ -1,5 +1,5 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
-//DEPS net.creativecouple.validation:fast-isbn:1.2.19
+//DEPS net.creativecouple.validation:fast-isbn:1.2.20
 
 import net.creativecouple.validation.isbn.ISBN;
 
