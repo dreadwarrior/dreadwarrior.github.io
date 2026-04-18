a2a725c6c28d7e0c49c3c17cfe768829683e2939
tåmm¥ <dreadwarrior@users.noreply.github.com>
2026-04-18 07:20:15 +0200

    Update fast-isbn

--- script/isbn13.java
+++ script/isbn13.java
@@ -1,5 +1,5 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
-//DEPS net.creativecouple.validation:fast-isbn:1.2.35
+//DEPS net.creativecouple.validation:fast-isbn:1.2.36
 
 import net.creativecouple.validation.isbn.ISBN;
 
